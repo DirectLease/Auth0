29a3db226cc542cfeaab4f4d7a37be9682b82d8e
Arno Bor <arno@directlease.nl>
2020-04-17 11:27:08 +0200

    Added config option for unverified members redirect to verification page

--- src/Trait/Verifiable.php
+++ src/Trait/Verifiable.php
@@ -2,20 +2,31 @@
 
 namespace DirectLease\Auth0;
 
+use SilverStripe\Core\Config\Config;
 use SilverStripe\Security\Security;
 
 trait Verifiable
 {
-
+    /**
+     * @return bool
+     */
     public function isMemberVerified()
     {
         $member = Security::getCurrentUser();
 
         if($member && !$member->auth0Verified)
         {
-            // TODO: Make config setting for URL if empty return a false and else true
-            return $this->redirect('verificatie/');
+            $url = Config::inst()->get(Verifiable::class, 'verification_uri');
+
+            if($url)
+            {
+                return $this->redirect($url);
+            }
+
+            return false;
         }
+
+        return true;
     }
 
 }
